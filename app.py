a347a1a8efca395c37a1c3c4ada86f215d158390
Mohammed Zayan <zayanit@gmail.com>
2021-06-29 23:17:10 +0300

    Implement missing fields Venue table

--- app.py
+++ app.py
@@ -36,14 +36,18 @@ class Venue(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    genres = db.Column(db.ARRAY(db.String))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    website_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String())
+    artists = db.relationship('Artist', secondary=shows,
+      backref=db.backref('venues', lazy=True))
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
