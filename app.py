88ae6692c4e90a00a9a08d0fd476ffd3e36d024e
Mohammed Zayan <zayanit@gmail.com>
2021-06-29 23:22:48 +0300

    Implement missing fields in Artist table

--- app.py
+++ app.py
@@ -60,6 +60,9 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String())
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
