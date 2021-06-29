25557953e2b269aaf51948d31506ab238880387f
Mohammed Zayan <zayanit@gmail.com>
2021-06-29 23:20:17 +0300

    Update Venue table columns

--- app.py
+++ app.py
@@ -36,14 +36,14 @@ class Venue(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    genres = db.Column(db.ARRAY(db.String))
+    genres = db.Column(db.String())
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    website_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String())
     artists = db.relationship('Artist', secondary=shows,
