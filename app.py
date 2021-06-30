518e9d4e3dd97cd876bc1a6846920f1cfaaa13e1
Mohammed Zayan <zayanit@gmail.com>
2021-06-30 22:17:03 +0300

    Implement show artists list

--- app.py
+++ app.py
@@ -227,18 +227,8 @@ def delete_venue(venue_id):
 #  ----------------------------------------------------------------
 @app.route('/artists')
 def artists():
-  # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
-  return render_template('pages/artists.html', artists=data)
+  artists = Artist.query.with_entities(Artist.id, Artist.name).all()
+  return render_template('pages/artists.html', artists=artists)
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
