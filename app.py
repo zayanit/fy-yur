e7915d6bb19d84b6d955d6f782887cc9dced560d
Mohammed Zayan <zayanit@gmail.com>
2021-06-30 18:48:36 +0300

    Update artist model

--- app.py
+++ app.py
@@ -68,16 +68,26 @@ class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     website = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean)
     seeking_description = db.Column(db.String())
+    shows = db.relationship('Show', backref='artist', lazy=True)
+
+    def __repr__(self):
+      artist = f'<Artist id: {self.id}, name: {self.name}, city: {self.city}, ' \
+        + f'state: {self.state}, phone: {self.phone}, genres: {self.genres}, ' \
+        + f'image_link: {self.image_link}, facebook_link: {self.facebook_link},' \
+        + f'website: {self.website}, genres: {self.genres}, ' \
+        + f'seeking_venue: {self.seeking_venue}, seeking_description: {self.seeking_description}, ' \
+        + f'shows: {self.shows}>\n'
+      return artist
 
 #----------------------------------------------------------------------------#
 # Filters.
