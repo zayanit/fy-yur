2020cddb484a7282d670aeba0439da2832be2219
Mohammed Zayan <zayanit@gmail.com>
2021-07-01 17:35:55 +0300

    Create show model

--- app.py
+++ app.py
@@ -26,17 +26,7 @@ migrate = Migrate(app, db)
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
-
-class Show(db.Model):
-  __tablename__ = 'show'
-
-  id = db.Column(db.Integer, primary_key=True)
-  venue_id = db.Column(db.Integer, db.ForeignKey('venue.id'), nullable=False)
-  artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
-  start_time = db.Column(db.DateTime, nullable=False)
-
-  def __repr__(self):
-    return f'<Show id: {self.id}, venue_id: {self.venue_id}, artist_id: {self.artist_id}, start_time: {self.start_time}>'
+from models.show import Show
 
 class Venue(db.Model):
     __tablename__ = 'venue'
